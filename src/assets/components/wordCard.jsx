2365fef7de8c9d005240a48a39de153a69c98f42
Aleksandra <alexandra.bakhcheva@gmail.com>
2022-11-03 00:05:22 +0300

    animation for wordcard component added

--- src/assets/components/wordCard.jsx
+++ src/assets/components/wordCard.jsx
@@ -1,4 +1,5 @@
 import "../styles/word_card.css";
+import { motion } from "framer-motion";
 import React, { useState } from "react";
 
 function WordCard(props) {
@@ -20,18 +21,21 @@ function WordCard(props) {
     };
 
     return (
-        <div className="wordcard">
+        <motion.div className="wordcard" animate={{
+            scale: [1, 1.2, 1.2, 1, 1],
+            rotate: [0, 360, 0],
+            borderRadius: ["20%", "50%", "20%", "50%", "20%"],}} transition={{ duration: 2}}>
             <div className="wordcard__box">
                 <h4 className="wordcard__close">X</h4>
                 <h4 className="wordcard__language">English</h4>
                 <h1 className="wordcard__word">{word}</h1>
                 <p className="wordcard__transcription">{transcription}</p>
                 <div onClick={showTranslation} className="wordcard__translation">
-                    <button className={hidden ? "hidden" : ""} >Translate</button>
+                    <motion.button className={hidden ? "hidden" : ""} whileHover={{ scale: 1.1}} whileTap={{scale: 0.9, x: "-5px", y: "5px"}} >Translate</motion.button>
                     <p className={translation ? "translation" : "hidden"}>{meaning}</p>
                 </div>
             </div>
-        </div>
+        </motion.div>
     );
 }
 
