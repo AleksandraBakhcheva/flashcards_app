9606217f52911208495c59bd8c92e3d29e66606b
Aleksandra <alexandra.bakhcheva@gmail.com>
2022-12-04 19:31:49 +0300

    404 gh pages

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Layout from "./assets/components/Layout";
 import NotFoundPage from "./assets/components/NotFoundPage";
 import WordslistTable from "./assets/components/WordsListTable";
 import WordsList from "./assets/components/WordsList";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
@@ -14,6 +14,7 @@ function App() {
           <Route path="Languages-Learn-Flashcards-Project" element={<WordslistTable />} />
           <Route path="game" element={<WordsList />} />
           <Route path="*" element={<NotFoundPage />} />
+          <Route path='*' element={<Navigate replace to='/404'/>} />
         </Route>
       </Routes>
     </>
