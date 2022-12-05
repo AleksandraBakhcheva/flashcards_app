2ab076702267c77dbe37585af909ec133789ef7f
Aleksandra <alexandra.bakhcheva@gmail.com>
2022-12-05 20:15:03 +0300

    project name changed

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/Languages-Learn-Flashcards-Project">
+    <BrowserRouter basename="/flashcards_app">
       <App />
     </BrowserRouter>
   </React.StrictMode>
