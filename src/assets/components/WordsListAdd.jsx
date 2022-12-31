b99f710fe69a3a775ab661c8c9b27b38246ec211
Aleksandra <alexandra.bakhcheva@gmail.com>
2022-12-31 15:04:40 +0300

    wordslistadd form validation

--- src/assets/components/WordsListAdd.jsx
+++ src/assets/components/WordsListAdd.jsx
@@ -1,5 +1,5 @@
 import "../styles/words_list_add.css";
-import WordsListItem from "./WordsListItem";
+//import WordsListItem from "./WordsListItem";
 import React, { useState, useRef } from "react";
 
 function WordsListAdd(props) {
@@ -9,33 +9,29 @@ function WordsListAdd(props) {
         translation: "",
     };
 
-    const [save, isSaved] = useState(false);
-    const [cancel, isCanceled] = useState(false);
+    //const [save, isSaved] = useState(false);
+    //const [cancel, isCanceled] = useState(false);
     const [values, setValues] = useState(initialValues);
+    const [errorMsg, setErrorMsg] = useState("");
+    const [successMsg, setSuccessMsg] = useState("");
     const wordRef = useRef();
     const translationRef = useRef();
-
-    const saveItem = () => {
-        if (values.word !== "" && values.translation !== "") {
-            isSaved(true);
-        }
-        
-    }
-
-    const cancelItem = () => {
-        isCanceled(!cancel);
-    }
 
     const addNewWord = (event) => {
         event.preventDefault();
         if (values.word === "" || values.translation === "") {
             values.word !== "" 
             ? translationRef.current.focus() 
-            : wordRef.current.focus()
+            : wordRef.current.focus();
+            setSuccessMsg("");
+            setErrorMsg("To add a word please fill in all fields");
             return null;
         }
-        else {
+        else if (validateWord(values.word) && validateTranslation(values.translation)) {
             setValues(initialValues);
+            setErrorMsg("");
+            setSuccessMsg("Your word has been successfully added");
+            console.log(values);
         }
     };
 
@@ -45,24 +41,49 @@ function WordsListAdd(props) {
             ...values,
             [name]: value,
         });
+        if (values.word) {
+            validateWord(values.word);
+        }
+        if (values.translation) {
+            validateTranslation(values.translation);
+        }
     };
 
     const validateWord = (userInput) => {
-        //setWord(userInput);
-        const engLetters = /^[a-zA-Z]$/;
-        if (engLetters.test(values.word) === true) {
-            console.log("correct");
+        setSuccessMsg("");
+        const engLetters = /^[A-Za-z]+$/gm;
+        if (!engLetters.test(userInput)) {
+            setErrorMsg("For the word field please use only latin characters");
+            wordRef.current.focus();
         }
         else {
-            console.log("please use only latin letters");
+            setErrorMsg("");
+            return true;
         }
-        
     }
 
     const validateTranslation = (userInput) => {
-        //setTranslation(userInput);
-        
+        setSuccessMsg("");
+        const rusLetters = /^[А-Яа-я]+$/gm;
+        if (!rusLetters.test(userInput)) {
+            setErrorMsg("For the translation field please use only cyrillic characters");
+            translationRef.current.focus();
+        }
+        else {
+            setErrorMsg("");
+            return true;
+        }
     }
+
+    /* const saveItem = () => {
+        if (values.word !== "" && values.translation !== "") {
+            isSaved(true);
+        }
+    }
+
+    const cancelItem = () => {
+        isCanceled(!cancel);
+    } */
 
     return (
         <div className="wordslist__item__new">
@@ -79,8 +100,12 @@ function WordsListAdd(props) {
                         <button className="wordslist__cancel">Cancel</button>
                 </form>
             </div>
-            {cancel ? <WordsListItem /> : null}
-            {save ? <WordsListItem word={values.word} translation={values.translation} /> : null}
+            <div>
+                {successMsg && <p className="wordslist__msg__success">{successMsg}</p>}
+                {errorMsg && <p className="wordslist__msg__error">{errorMsg}</p>}
+            </div>
+            {/* {cancel ? <WordsListItem /> : null}
+            {save ? <WordsListItem word={values.word} translation={values.translation} /> : null} */}
             {/* <tr className={"wordslist__item__box_add" + (cancel ? " hidden" : "")}></tr> */}
         </div>
     );
