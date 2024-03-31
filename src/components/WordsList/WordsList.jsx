7ecece09b6a719da21f84628004a8afe92a47f9a
Aleksandra <alexandra.bakhcheva@gmail.com>
2024-03-31 17:07:35 +0300

    add error if context is undefined

--- src/components/WordsList/WordsList.jsx
+++ src/components/WordsList/WordsList.jsx
@@ -1,5 +1,6 @@
 import styles from "./WordsList.module.css";
 import { WordCard } from "../WordCard/WordCard";
+import { NoWordsError } from "../NoWordsError/NoWordsError";
 import { GeneralContext } from "../../contexts/GeneralContext";
 import { useContext, useState, useRef } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -7,14 +8,10 @@ import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import { CircularProgress } from "react-loading-indicators";
 
 export const WordsList = () => {
-  const { context, setContext, loading, errorMsg } = useContext(GeneralContext);
+  const { context, setContext, loading, errorMsg, learned, updateLearned } =
+    useContext(GeneralContext);
   const [index, setIndex] = useState(0);
-  const [learned, isLearned] = useState(0);
   const ref = useRef();
-
-  if (!context) {
-    return;
-  }
 
   const showPrevCard = () => {
     if (index === 0) {
@@ -35,56 +32,62 @@ export const WordsList = () => {
   };
 
   const calculateLearned = () => {
-    isLearned(learned + 1);
+    updateLearned();
     const wordsUpdate = [...context];
     wordsUpdate[index].isClicked = true;
     setContext(wordsUpdate);
   };
 
   return (
-    <div className={styles.container}>
-      {loading ? (
-        <div className={styles.loading}>
-          <CircularProgress
-            variant="bubble-dotted"
-            size="large"
-            text="LOADING"
-            color="#b40101"
-          />
-          <div>{errorMsg}</div>
-        </div>
-      ) : (
-        <>
-          <button
-            onClick={showPrevCard}
-            className={`${styles.button} ${styles.arrow_left}`}
-          >
-            <FontAwesomeIcon icon={faArrowLeft} className={styles.icon} />
-          </button>
-          {index === undefined ? (
-            <WordCard />
+    <>
+      {context.length > 0 ? (
+        <div className={styles.container}>
+          {loading ? (
+            <div className={styles.loading}>
+              <CircularProgress
+                variant="bubble-dotted"
+                size="large"
+                text="LOADING"
+                color="#b40101"
+              />
+              <div>{errorMsg}</div>
+            </div>
           ) : (
             <>
-              <WordCard
-                word={context[index].word}
-                transcription={context[index].transcription}
-                translation={context[index].translation}
-                learned={learned}
-                onClick={calculateLearned}
-                isClicked={context[index].isClicked}
-                ref={ref}
-                index={index + 1 + "/" + context.length}
-              />
+              <button
+                onClick={showPrevCard}
+                className={`${styles.button} ${styles.arrow_left}`}
+              >
+                <FontAwesomeIcon icon={faArrowLeft} className={styles.icon} />
+              </button>
+              {index === undefined ? (
+                <WordCard />
+              ) : (
+                <>
+                  <WordCard
+                    word={context[index].word}
+                    transcription={context[index].transcription}
+                    translation={context[index].translation}
+                    learned={learned}
+                    onClick={calculateLearned}
+                    isClicked={context[index].isClicked}
+                    ref={ref}
+                    index={index + 1 + "/" + context.length}
+                  />
+                </>
+              )}
+              <button
+                onClick={showNextCard}
+                className={`${styles.button} ${styles.arrow_right}`}
+              >
+                <FontAwesomeIcon icon={faArrowRight} className={styles.icon} />
+              </button>
             </>
           )}
-          <button
-            onClick={showNextCard}
-            className={`${styles.button} ${styles.arrow_right}`}
-          >
-            <FontAwesomeIcon icon={faArrowRight} className={styles.icon} />
-          </button>
-        </>
+        </div>
+      ) : (
+        <NoWordsError />
       )}
-    </div>
+    </>
   );
 };
